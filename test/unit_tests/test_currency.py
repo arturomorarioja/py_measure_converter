121302e9280ee0508edeb3e0c32df0e2b4eeb2c6
Arturo Mora-Rioja <arturomora@gmail.com>
2024-09-09 17:19:19 +0200

    Currency unit test added

---
+++ test/unit_tests/test_currency.py
@@ -0,0 +1,15 @@
+import pytest
+from api.classes.currency import Currency
+
+@pytest.fixture(scope='function')
+def currency_fixture(mocker):
+    currency = Currency()
+    mocker.patch.object(currency, 'convert', return_value=1000)
+    return currency
+
+class TestGrading():
+    
+    def test_convert_returns_float(self, currency_fixture):
+        assert currency_fixture.convert(1000) == 1000
+
+    # Not very useful
